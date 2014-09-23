68a549e2cbc5b99087efe0d919dc6cced691a5af
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-09-23 16:00:27 +0200

    Update phabricator.pp

--- role/phabricator.pp
+++ role/phabricator.pp
@@ -207,7 +207,7 @@ class role::phabricator::sprint {
        load_path => "/usr/share/augeas/lenses/dist/",
        context => "/files/etc/apache2/sites-available/50-phabricator.conf",
        incl => "/files/etc/apache2/sites-available/50-phabricator.conf",
-       lens => "httpd.aug",
+       lens => "/usr/share/augeas/lenses/dist/httpd.aug",
        changes => [
        "set VirtualHost/directive SetEnv",
        "set VirtualHost/*[self::directive='SetEnv']/arg[1] PHABRICATOR_ENV",
