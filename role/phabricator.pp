29ca117878d0fe0ed684c0ecee3152d47f9e4a7b
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-09-25 11:54:30 +0200

    Update phabricator.pp

--- role/phabricator.pp
+++ role/phabricator.pp
@@ -139,7 +139,7 @@ class role::phabricator::labs {
             'phabricator.show-beta-applications' => true,
             'mysql.pass'                         => $mysqlpass,
             'auth.require-email-verification'    => false,
-            'load-libraries'                     => {'burndown':'/srv/phab/extensions/Sprint'}
+            'load-libraries'                     => {'burndown':'/srv/phab/extensions/Sprint'},
         },
     }
 
